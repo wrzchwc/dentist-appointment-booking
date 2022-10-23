85376ba09aa8d657790a6fc8f7c2af44d41ce5f7
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-10-23 23:48:13 +0200

    Created AppointmentFact model

---
+++ server/src/models/appointment-fact.model.ts
@@ -0,0 +1,15 @@
+import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from 'sequelize';
+import { sequelizeInstance } from '../services';
+
+export class AppointmentFact extends Model<InferAttributes<AppointmentFact>, InferCreationAttributes<AppointmentFact>> {
+    declare id: CreationOptional<string>;
+    declare value: string;
+}
+
+AppointmentFact.init(
+    {
+        id: { type: DataTypes.STRING, primaryKey: true, allowNull: false, defaultValue: DataTypes.UUIDV4 },
+        value: { type: DataTypes.STRING, allowNull: false, unique: true },
+    },
+    { timestamps: false, sequelize: sequelizeInstance, tableName: 'appointment_facts' }
+);
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,2 +1,3 @@
 export * from './user.model';
 export * from './service.model';
+export * from './appointment-fact.model';
