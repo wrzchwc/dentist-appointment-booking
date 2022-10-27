ffdc400a3e5ab6044682b23d9a089a949d0331fa
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-10-27 20:57:41 +0200

    Implemented home component

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, HomeRoutingModule, MatButtonModule],
+    imports: [CommonModule, HomeRoutingModule, MatButtonModule, MatIconModule],
 })
 export class HomeModule {}
