26d9bb456a9a30cfeddbf6c992e8ad54ed18da9d
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-10-29 18:41:05 +0200

    Finished header impl

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { AdminModule } from '../admin/admin.module';
 
 @NgModule({
     declarations: [HeaderComponent],
     exports: [HeaderComponent],
-    imports: [CommonModule, MatButtonModule, MatIconModule, RouterLink],
+    imports: [CommonModule, MatButtonModule, MatIconModule, RouterLink, AdminModule],
 })
 export class SharedModule {}
