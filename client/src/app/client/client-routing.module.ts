b5e63e93a2870c3d37ee09e0ad788d59c0a1e99a
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-10-29 19:30:53 +0200

    Client routes

--- client/src/app/client/client-routing.module.ts
+++ client/src/app/client/client-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from './client/client.component';
+import { ClientAppointmentsComponent } from './client-appointments/client-appointments.component';
+import { AppointmentBookingComponent } from './appointment-booking/appointment-booking.component';
 
-const routes: Routes = [{ path: '', component: ClientComponent, title: 'Panel klienta' }];
+const routes: Routes = [
+    { path: 'appointments', component: ClientAppointmentsComponent, title: 'Moje wizyty' },
+    { path: 'booking', component: AppointmentBookingComponent, title: 'Rezerwacja wizyty' },
+    { path: '', component: ClientComponent, title: 'Panel klienta' },
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
--- client/src/app/client/client.module.ts
+++ client/src/app/client/client.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientComponent } from './client/client.component';
+import { ClientAppointmentsComponent } from './client-appointments/client-appointments.component';
+import { AppointmentBookingComponent } from './appointment-booking/appointment-booking.component';
 
 @NgModule({
-    declarations: [ClientComponent],
+    declarations: [ClientComponent, ClientAppointmentsComponent, AppointmentBookingComponent],
     imports: [CommonModule, ClientRoutingModule],
 })
 export class ClientModule {}
