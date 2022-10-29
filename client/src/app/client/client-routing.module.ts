21aeec281f0dfcf473a6572c99c5f6952565d3d9
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-10-30 00:12:25 +0200

    renamed /client/booking to /client/appointment-booking

--- client/src/app/client/client-routing.module.ts
+++ client/src/app/client/client-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from './client/client.component';
 import { ClientAppointmentsComponent } from './client-appointments/client-appointments.component';
-import { AppointmentBookingComponent } from './booking/appointment-booking/appointment-booking.component';
+import { AppointmentBookingComponent } from './appointment-booking/appointment-booking/appointment-booking.component';
 import { ServicesResolver } from '../shared/_resolvers/appointment-data/services.resolver';
-import { AppointmentQuestionsResolver } from './booking/_resolvers/appointment-data/appointment-questions.resolver';
+import { AppointmentQuestionsResolver } from './appointment-booking/_resolvers/appointment-data/appointment-questions.resolver';
 
 const routes: Routes = [
     { path: 'appointments', component: ClientAppointmentsComponent, title: 'Moje wizyty' },
--- client/src/app/client/client.module.ts
+++ client/src/app/client/client.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientComponent } from './client/client.component';
 import { ClientAppointmentsComponent } from './client-appointments/client-appointments.component';
-import { AppointmentBookingModule } from './booking/appointment-booking.module';
+import { AppointmentBookingModule } from './appointment-booking/appointment-booking.module';
 
 @NgModule({
     declarations: [ClientComponent, ClientAppointmentsComponent],
