87fa57791a9add33527c5b7a6841faaea41086a7
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-10-30 01:30:28 +0200

    Implemented stepper header

--- client/src/app/client/appointment-booking/appointment-booking.module.ts
+++ client/src/app/client/appointment-booking/appointment-booking.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppointmentBookingComponent } from './appointment-booking/appointment-booking.component';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
     declarations: [AppointmentBookingComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MatStepperModule],
     exports: [AppointmentBookingComponent],
 })
 export class AppointmentBookingModule {}
