59ad2303d91d6163b50f70ae5b0ebb5ddd764dd8
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-10-30 02:11:45 +0200

    Added step buttons

--- client/src/app/client/appointment-booking/appointment-booking.module.ts
+++ client/src/app/client/appointment-booking/appointment-booking.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppointmentBookingComponent } from './appointment-booking/appointment-booking.component';
 import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
     declarations: [AppointmentBookingComponent],
-    imports: [CommonModule, MatStepperModule],
+    imports: [CommonModule, MatStepperModule, MatButtonModule, RouterLink],
     exports: [AppointmentBookingComponent],
 })
 export class AppointmentBookingModule {}
