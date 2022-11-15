26f484b1fb6ff29e0527ef6fc34aed310e18ed9c
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-11-15 14:11:31 +0100

    Created tooltip service. Code refactoring

--- client/src/app/appointment-booking/service-card/service-card.component.ts
+++ client/src/app/appointment-booking/service-card/service-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { Service } from 'src/app/shared/_services/appointments/services.service';
+import { TooltipService } from './tooltip.service';
 
 @Component({
     selector: 'app-service-card',
@@ -10,27 +11,12 @@ export class ServiceCardComponent implements OnChanges {
     @Input() service?: Service;
     tooltip: string;
 
-    constructor() {
+    // eslint-disable-next-line no-unused-vars
+    constructor(private tooltipService: TooltipService) {
         this.tooltip = '';
     }
 
     ngOnChanges() {
-        if (!this.service || this.service.detail === 'C') {
-            return;
-        }
-        this.setTooltip(this.service.detail);
-    }
-
-    private setTooltip(detail: 'A' | 'B' | null) {
-        switch (detail) {
-            case 'A':
-                this.tooltip = '1 ząb: 170 PLN, 2 zęby 250 PLN, 3 zęby 300 PLN, kolejny ząb: +50 PLN';
-                break;
-            case 'B':
-                this.tooltip = '1 punkt: 170 PLN, kolejny punkt: +30 PLN';
-                break;
-            default:
-                return;
-        }
+        this.tooltip = this.tooltipService.getTooltip(this.service?.detail);
     }
 }
