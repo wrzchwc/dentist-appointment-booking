dc60399c0aaf9c3b12341f5df602b64698f1e03b
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-11-15 12:57:53 +0100

    Creating surveys on AppointmentBookingComponent being constructed

--- client/src/app/appointment-booking/appointment-booking/appointment-booking.component.ts
+++ client/src/app/appointment-booking/appointment-booking/appointment-booking.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Service } from '../../shared/_services/appointments/services.service';
 import { AppointmentQuestion } from '../_services/appointment-questions/appointment-questions.service';
 import { AppointmentTimeService } from '../_services/appointment-time/appointment-time.service';
+import { AppointmentsService } from '../../shared/_services/appointments/appointments.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'app-appointment-booking',
@@ -12,14 +14,25 @@ import { AppointmentTimeService } from '../_services/appointment-time/appointmen
 export class AppointmentBookingComponent implements OnDestroy {
     readonly services: Service[];
     readonly questions: AppointmentQuestion[];
+    private readonly onDestroy: Subject<null>;
 
-    // eslint-disable-next-line no-unused-vars
-    constructor(private route: ActivatedRoute, private time: AppointmentTimeService) {
+    constructor(
+        private route: ActivatedRoute,
+        // eslint-disable-next-line no-unused-vars
+        private time: AppointmentTimeService,
+        private appointments: AppointmentsService
+    ) {
         this.services = route.snapshot.data['services'];
         this.questions = route.snapshot.data['appointmentQuestions'];
+        this.onDestroy = new Subject<null>();
+        appointments
+            .createAppointment()
+            .pipe(takeUntil(this.onDestroy))
+            .subscribe((appointment) => (appointments.currentAppointment = appointment));
     }
 
     ngOnDestroy(): void {
         this.time.selectedTime$.next(null);
+        this.onDestroy.next(null);
     }
 }
--- client/src/app/shared/_services/appointments/appointments.service.ts
+++ client/src/app/shared/_services/appointments/appointments.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Service } from './services.service';
+
+type AssociatedService = Service & { appointmentServices: { quantity: number } };
+
+interface Appointment {
+    id: string;
+    startsAt: Date;
+    services: AssociatedService[];
+}
 
 @Injectable({
     providedIn: 'root',
 })
 export class AppointmentsService {
     readonly baseUrl: string;
+    currentAppointment?: Appointment;
 
-    constructor() {
+    // eslint-disable-next-line no-unused-vars
+    constructor(private client: HttpClient) {
         this.baseUrl = `${environment.apiUrl}/api/appointments`;
+    }
+
+    createAppointment() {
+        return this.client.post<Appointment>(this.baseUrl, {});
     }
 }
