a9635eca7c8695c75f36be3cc53fd9b43719e83d
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-11-17 16:47:39 +0100

    Temporarily switched GET /api/services to return services with `count` equal to 1. Added error handling to GET /api/services

--- server/src/api/services/services.controller.ts
+++ server/src/api/services/services.controller.ts
@@ -2,6 +2,13 @@ import { Request, Response } from 'express';
 import { Service } from '../../models';
 
 export async function getServices(request: Request, response: Response) {
-    const services = await Service.findAll();
+    let services: Service[];
+
+    try {
+        services = await Service.findAll({ where: { count: 1 }, order: [['price', 'ASC']] });
+    } catch (e) {
+        return response.status(500).json({ error: 'Operation failed' });
+    }
+
     response.status(200).json(services);
 }
