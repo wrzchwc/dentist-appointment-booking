85300fe6db30691daea889be2077a12e79e3ea6e
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-11-25 01:48:49 +0100

    Revert "Refactored AppointmentsComponent"
    This reverts commit 5da4d2df58c9b284c9c637a61189192e308f4726.

--- client/src/app/shared/appointments/appointments.component.ts
+++ client/src/app/shared/appointments/appointments.component.ts
@@ -10,7 +10,7 @@ import { DateService } from '../_services/utility/date.service';
 })
 export class AppointmentsComponent implements OnChanges, OnDestroy {
     @Input() listTitle?: string;
-    @Input() empty: boolean;
+    @Input() appointments?: Appointment[];
     @Output() readonly dateChange: EventEmitter<Date>;
     readonly pickerControl: FormControl<Date>;
     private readonly onDestroy: Subject<void>;
@@ -19,7 +19,6 @@ export class AppointmentsComponent implements OnChanges, OnDestroy {
         this.onDestroy = new Subject<void>();
         this.dateChange = new EventEmitter<Date>();
         this.pickerControl = builder.control(date.currentWorkday, { nonNullable: true });
-        this.empty = true;
     }
 
     ngOnChanges() {
