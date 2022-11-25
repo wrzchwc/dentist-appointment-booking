4f5ca37246f5bd8d85b0f8faf87eb99f0d2f3c00
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-11-25 20:38:05 +0100

    Refactored PATCH /api/appointments

--- server/src/api/appointments/appointments.router.ts
+++ server/src/api/appointments/appointments.router.ts
@@ -9,7 +9,7 @@ router.get('/questions', controller.getQuestions);
 router.get('/me/:appointmentId', controller.getClientAppointment);
 router.delete('/me/:appointmentId', controller.deleteClientAppointment);
 router.get('/me', controller.getClientAppointments);
-router.patch('/:appointmentId/starts-at', controller.updateAppointmentStartDate);
+router.patch('/:appointmentId', controller.updateAppointment);
 router.get('/:appointmentId', authorisation, controller.getAppointment);
 router.delete('/:appointmentId', authorisation, controller.deleteAppointment);
 router.get('/', authorisation, controller.getAppointments);
