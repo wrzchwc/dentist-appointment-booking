4b84a0e94a7ccb8f64e69a3e211ac7558312faf4
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-11-25 02:39:49 +0100

    Created AppointmentService

--- client/src/app/client/client-appointment/client-appointment.component.ts
+++ client/src/app/client/client-appointment/client-appointment.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Appointment, AssociatedService } from '../client-appointments/client-appointments.service';
+import { Appointment } from '../client-appointments/client-appointments.service';
 import { PriceService } from '../../shared/_services/utility/price.service';
-import { DateService } from '../../shared/_services/utility/date.service';
-import { LengthItem, LengthService } from '../../shared/_services/utility/length.service';
 import { NamedPriceItem } from '../../shared/table/services-table.component';
 import { ClientAppointmentService } from './client-appointment.service';
 import { Subject, takeUntil } from 'rxjs';
 import { Location } from '@angular/common';
+import { AppointmentService } from '../../shared/appointment/appointment.service';
 
 @Component({
     selector: 'app-client-appointment',
@@ -26,19 +25,18 @@ export class ClientAppointmentComponent implements OnDestroy {
     constructor(
         private activatedRoute: ActivatedRoute,
         private priceService: PriceService,
-        private dateService: DateService,
-        private lengthService: LengthService,
         // eslint-disable-next-line no-unused-vars
         private clientAppointmentService: ClientAppointmentService,
         // eslint-disable-next-line no-unused-vars
-        private location: Location
+        private location: Location,
+        private appointmentService: AppointmentService
     ) {
         this.appointment = activatedRoute.snapshot.data['appointment'];
-        this.dataSource = this.mapServicesToPriceItems();
+        this.dataSource = appointmentService.createDateSource(this.appointment.services);
         this.price = priceService.calculateTotalPrice(this.dataSource);
-        this.length = lengthService.calculateTotalLength(this.mapServicesToLengthItems());
-        this.cancelable = dateService.currentDay < new Date(this.appointment.startsAt);
-        this.endsAt = this.calculateEndsAt();
+        this.length = appointmentService.calculateLength(this.appointment.services);
+        this.cancelable = appointmentService.isCancelable(this.appointment.startsAt);
+        this.endsAt = appointmentService.calculateEndsAt(this.appointment.startsAt, length);
         this.onDestroy = new Subject<void>();
     }
 
@@ -53,25 +51,5 @@ export class ClientAppointmentComponent implements OnDestroy {
             .subscribe(() => {
                 this.location.back();
             });
-    }
-
-    private mapServicesToPriceItems(): NamedPriceItem[] {
-        return this.appointment.services.map(this.mapServiceToPriceItem);
-    }
-
-    private mapServiceToPriceItem({ price, appointmentServices, detail, name }: AssociatedService): NamedPriceItem {
-        return { quantity: appointmentServices.quantity, detail, name, price };
-    }
-
-    private mapServicesToLengthItems(): LengthItem[] {
-        return this.appointment.services.map(this.mapServiceToLengthItem);
-    }
-
-    private mapServiceToLengthItem(service: AssociatedService): LengthItem {
-        return { quantity: service.appointmentServices.quantity, length: service.length };
-    }
-
-    private calculateEndsAt(): Date {
-        return new Date(new Date(this.appointment.startsAt).setMinutes(this.length));
     }
 }
