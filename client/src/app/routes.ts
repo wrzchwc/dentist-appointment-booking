9834e0d4d26852762bc3e34023cb073ab4a2b3b0
Jakub Wierzchowiec <wierzchowieckuba@gmail.com>
2023-10-07 01:50:00 +0200

    finished application conversion to standalone

---
+++ client/src/app/routes.ts
@@ -0,0 +1,31 @@
+import { Routes } from '@angular/router';
+import { AuthenticationGuard } from './shared/guards/authentication.guard';
+import { AuthenticationResolver } from './shared/resolvers/authentication.resolver';
+
+export const APP_ROUTES: Routes = [
+    {
+        path: 'client',
+        children: [
+            {
+                path: 'appointment-booking',
+                canLoad: [AuthenticationGuard],
+                loadChildren: async () => (await import('./appointment-booking/routes')).APPOINTMENT_BOOKING_ROUTES,
+            },
+            {
+                path: '',
+                canLoad: [AuthenticationGuard],
+                loadChildren: async () => (await import('./client/routes')).CLIENT_ROUTES,
+            },
+        ],
+    },
+    {
+        path: 'admin',
+        canLoad: [AuthenticationGuard],
+        loadChildren: async () => (await import('./admin/routes')).ADMIN_ROUTES,
+    },
+    {
+        path: '',
+        loadComponent: async () => (await import('./shared/components/page/home/home.component')).HomeComponent,
+        resolve: { profile: AuthenticationResolver },
+    },
+];
